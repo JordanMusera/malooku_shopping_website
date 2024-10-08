822985acf2df090de5de05a352653d1eaefb2145
Jordan Musera <jordanmusera@gmail.com>
2024-10-08 16:44:32 +0300

    4:44pm - 8/10/2024

--- my-app/src/app/components/LoginContainer.tsx
+++ my-app/src/app/components/LoginContainer.tsx
@@ -1,12 +1,16 @@
 'use client'
+import Link from 'next/link';
 import React, { useState } from 'react'
+import { ToastContainer,toast } from 'react-toastify';
+import 'react-toastify/ReactToastify.css'
 
 const LoginContainer = () => {
 const[email,setEmail] = useState('');
 const[password,setPassword] = useState('');
 
-const loginFunction=async()=>{
-const res = await fetch('/api/login',{
+const loginFunction=async(e: { preventDefault: () => void; })=>{
+  e.preventDefault()
+const res = await fetch('/api/authenticate/login',{
     method:'POST',
     headers:{
         'Content-Type':'application/json'
@@ -17,10 +21,20 @@ const res = await fetch('/api/login',{
     })
 
 })
+
+const response = await res.json();
+
+if(response.success){
+  toast.success(response.message);
+}else{
+  toast.error(response.message);
+}
+
 }
 
   return (
-    <form className='w-3/4 p-3 rounded-lg bg-pink-300 h-max flex flex-col text-black gap-1'>
+    <div className='w-3/4'>
+      <form className='w-full p-3 rounded-lg bg-pink-300 h-max flex flex-col text-black gap-1' onSubmit={loginFunction}>
         <label className='flex justify-center w-full font-bold text-xl text-white'>LOGIN PAGE</label>
         <label>Email</label>
       <input type='email' placeholder='Enter email' className='rounded h-10 px-2'
@@ -28,11 +42,14 @@ const res = await fetch('/api/login',{
       <label>Password</label>
       <input type='password' placeholder='Enter password' className='rounded h-10 px-2'
       onChange={e=>setPassword(e.target.value)}/>
-      <p><span className='text-black font-md'>Forgot password? </span><span className='text-blue-500 font-semibold text-md'>Reset</span></p>
-      <button className='bg-gray-400 w-full h-10 text-xl text-white font-bold flex justify-center items-center rounded-md my-2'
-      onClick={()=>loginFunction()}>LOGIN</button>
-      <p><span className='text-black font-md'>Don't have an account? </span><span className='text-blue-500 font-semibold text-md'>Sign up</span></p>
+      <p><span className='text-black font-md'>Forgot password? </span><Link href='/resetPassword' className='text-blue-500 font-semibold text-md'>Reset</Link></p>
+      <button className='bg-gray-400 w-full h-10 text-xl text-white font-bold flex justify-center items-center rounded-md my-2'>LOGIN</button>
+      <p><span className='text-black font-md'>Don't have an account? </span><Link href='/signup' className='text-blue-500 font-semibold text-md'>Sign up</Link></p>
     </form>
+
+    <ToastContainer/>
+    </div>
+    
   )
 }
 
--- my-app/src/app/components/SignupContainer.tsx
+++ my-app/src/app/components/SignupContainer.tsx
@@ -1,31 +1,49 @@
 'use client'
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import React, { useState } from 'react'
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const SignupContainer = () => {
     const[username,setUsername] = useState('');
     const[email,setEmail] = useState('');
     const[password,setPassword] = useState('');
+
+    const router = useRouter();
     
-    const loginFunction=async()=>{
+    const loginFunction=async(e: { preventDefault: () => void; })=>{
+        e.preventDefault();
     const res = await fetch('/api/authenticate/signup',{
         method:'POST',
         headers:{
             'Content-Type':'application/json'
         },
         body:JSON.stringify({
-            username:username,
+            userName:username,
             userEmail:email,
             userPassword:password
-        })
-    
+        })    
     })
+
+    const response = await res.json();
+
+    if (response.success) {
+      toast.success(response.message);
+      if(response.message==="Enter Otp code sent to your email"){
+        router.push(`signup/verifyEmail/${email}`)
+      }
+    } else {
+      toast.error(response.message);
     }
+  }
 
   return (
-    <form className='w-3/4 p-3 rounded-lg bg-pink-300 h-max flex flex-col text-black gap-1'>
+    <div className='w-3/4'>
+       <form className='w-full p-3 rounded-lg bg-pink-300 h-max flex flex-col text-black gap-1' onSubmit={loginFunction}>
     <label className='flex justify-center w-full font-bold text-xl text-white'>SIGNUP PAGE</label>
     <label>Username</label>
-  <input type='email' placeholder='Enter username' className='rounded h-10 px-2'
+  <input type='text' placeholder='Enter username' className='rounded h-10 px-2'
   onChange={e=>setUsername(e.target.value)}/>
     <label>Email</label>
   <input type='email' placeholder='Enter email' className='rounded h-10 px-2'
@@ -33,11 +51,13 @@ const SignupContainer = () => {
   <label>Password</label>
   <input type='password' placeholder='Enter password' className='rounded h-10 px-2'
   onChange={e=>setPassword(e.target.value)}/>
-  <p><span className='text-black font-md'>Forgot password? </span><span className='text-blue-500 font-semibold text-md'>Reset</span></p>
-  <button className='bg-gray-400 w-full h-10 text-xl text-white font-bold flex justify-center items-center rounded-md my-2'
-  onClick={()=>loginFunction()}>SIGN UP</button>
-  <p><span className='text-black font-md'>Don't have an account? </span><span className='text-blue-500 font-semibold text-md'>Sign up</span></p>
+  <button className='bg-gray-400 w-full h-10 text-xl text-white font-bold flex justify-center items-center rounded-md my-3' type='submit'>PROCEED</button>
+  <p><span className='text-black font-md'>Don't have an account? </span><Link href='/login' className='text-blue-500 font-semibold text-md'>Login</Link></p>
 </form>
+
+<ToastContainer/>
+    </div>
+   
 )
 }
 
