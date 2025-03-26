64ae74aa4ce13406b0e303d43cc529ccb9efd23b
Jordan Musera <jordanmusera@gmail.com>
2025-03-27 01:09:02 +0300

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
     images: {
         domains: ['fakestoreapi.com','res.cloudinary.com','th.bing.com'], // Allow images from this domain
       },
+      experimental: {
+        runtime: "nodejs",
+      }
 };
 
 export default nextConfig;
